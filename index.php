16b95f847c895b81e00bfa065c85f49359b711b2
John Stafford <john@jkms.me>
2015-11-23 10:55:26 -0800

    Attempting SQL

--- index.php
+++ index.php
@@ -5,12 +5,52 @@
   <body>
 
  <?php
+ $SQL_servername = "localhost";
+ $SQL_username = "username";
+ $SQL_password = "password";
+ $SQL_dbname = "myDB";
+function create_customerdb(
+  $SQL_servername,
+  $SQL_password,
+  $SQL_username,
+  $SQL_dbname)
+{
+  $SQL_connection = mysqli_connect(
+    $SQL_servername,
+    $SQL_password,
+    $SQL_username,
+    $SQL_dbname
+  );
+
+  if ($SQL_connection) {die("Connection failed: " . mysqli_connect_error());}
+
+  // sql to create table
+  $sql = "CREATE TABLE MyGuests (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  firstname VARCHAR(30) NOT NULL,
+  lastname VARCHAR(30) NOT NULL,
+  email VARCHAR(50),
+  reg_date TIMESTAMP
+  )";
+
+  if (mysqli_query($conn, $sql)) {
+      echo "Table MyGuests created successfully";
+  } else {
+      echo "Error creating table: " . mysqli_error($conn);
+  }
+
+  mysqli_close($conn);
+}
+
+  create_customerdb($SQL_servername,
+  $SQL_password,
+  $SQL_username,
+  $SQL_dbname);
+  
   $MyVariable = "Hello World";
   echo $MyVariable;
   echo "A bunch of changes";
  ?>
-
- $Something = "This is a test";
 
  </BODY>
 </html>
