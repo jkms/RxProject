7f43b9cc9d83a66f9a1c25cb7a41c2ebaa4b6a8d
John Stafford <john@jkms.me>
2015-11-24 14:48:56 -0800

    Added links

--- theme/header.php
+++ theme/header.php
@@ -8,7 +8,9 @@ echo "<html>
 <body>
 <h1>R<sub>X</sub>Project</h1><br<br>
 
-<a href=#>Home</a> | <a href=index.php?function=install>Install</a> |
-<a href=index.php?function=reset>Reset</a> | <a href=#>Option 4</a>  |
-<a href=#>Options 5</a><br><br>";
+<a href=index.php>Home</a> |
+<a href=index.php?function=install>Install</a> |
+<a href=index.php?function=reset>Reset</a> |
+<a href=index.php?function=customers>Customers</a>  |
+<a href=index.php?function=drugs>Drugs</a><br><br>";
 ?>
