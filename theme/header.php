ce828e302ac1eb14260906766cb892196e01a474
John Stafford <john@jkms.me>
2015-11-24 15:57:55 -0800

    Updated logo. Ghetto

--- theme/header.php
+++ theme/header.php
@@ -1,13 +1,19 @@
 <?php
 $title = "RxProject";
 
-echo "<html>
+echo "<!DOCTYPE html>
+<html>
+<head>
+<style>
+  RX {
+    letter-spacing: -16px;
+  }
+</style>
 <head>
 <title>$title</title>
 </head>
 <body>
-<h1>R<sub>X</sub>Project</h1><br<br>
-
+<h1><RX>R<sub>X</RX></sub> Project</h1><br<br>
 <a href=index.php>Home</a> |
 <a href=index.php?function=install>Install</a> |
 <a href=index.php?function=reset>Reset</a> |
